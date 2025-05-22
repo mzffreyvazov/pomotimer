2be22a3bdfe5d20f5c3cd7810725ebe4039566e4
mzffreyvazov <mzffreyvazov2005@gmail.com>
2025-05-22 19:48:38 +0400

    Refactor Toaster component

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from "@/contexts/ThemeContext"
 import { Toaster as Sonner, toast } from "sonner"
 
+// Use Poppins as default font, match border, background, and shadow to design system
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
 const Toaster = ({ ...props }: ToasterProps) => {
@@ -12,16 +13,19 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={sonnerTheme as ToasterProps["theme"]}
-      className="toaster group font-inter font-[200]"
+      className="toaster group font-inter font-medium"
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
+            // Card style: rounded, border, bg, shadow, transition
+            "text-[13.5px] group toast group-[.toaster]:bg-pomo-background group-[.toaster]:text-pomo-foreground group-[.toaster]:border-pomo-muted/30 group-[.toaster]:border group-[.toaster]:rounded-2xl group-[.toaster]:shadow-lg group-[.toaster]:shadow-black/30 group-[.toaster]:transition-all group-[.toaster]:duration-300 group-[.toaster]:ease-in-out px-6 py-4",
+          description: "group-[.toast]:text-pomo-secondary text-[12px] font-medium ",
           actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+            // Primary button style
+            "group-[.toast]:bg-pomo-primary/80 group-[.toast]:hover:bg-pomo-primary group-[.toast]:text-pomo-background group-[.toast]:rounded-lg group-[.toast]:transition-colors group-[.toast]:duration-300 px-4 py-2 font-semibold",
           cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+            // Ghost button style
+            "group-[.toast]:bg-pomo-muted/40 group-[.toast]:text-pomo-secondary group-[.toast]:rounded-lg group-[.toast]:transition-colors group-[.toast]:duration-300 px-4 py-2 font-medium",
         },
       }}
       {...props}
